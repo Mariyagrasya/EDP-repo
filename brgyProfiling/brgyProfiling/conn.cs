a94221d92c5501f1afc808ab2a9dc52c166eb002
Mariyagrasya <mgcc2022-5098-58918@bicol-u.edu.ph>
2025-05-05 15:42:54 +0800

    init

---
+++ brgyProfiling/brgyProfiling/conn.cs
@@ -0,0 +1,27 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+
+namespace brgyProfiling
+{
+    internal class conn
+    {
+        // Connection string (update with your database details)
+        private static string connectionString = "Server=localhost;Database=brgyprofilingsys;User ID=root;Password=159632;";
+
+        // Method to get a MySQL connection
+        public static MySqlConnection GetConnection()
+        {
+            try
+            {
+                MySqlConnection connection = new MySqlConnection(connectionString);
+                connection.Open();
+                return connection;
+            }
+            catch (MySqlException ex)
+            {
+                Console.WriteLine("Database connection error: " + ex.Message);
+                throw;
+            }
+        }
+    }
+}
